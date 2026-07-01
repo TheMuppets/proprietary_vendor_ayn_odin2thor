a2c6680eff0ef538dbe93cafc3c2a4a4b1ddaedd
AnierinB <anierin@evolution-x.org>
2026-07-01 02:27:33 +0000

    odin2thor: Import blobs & firmware from Thor.20251120.235628
    Signed-off-by: AnierinB <anierin@evolution-x.org>

---
+++ Android.mk
@@ -0,0 +1,31 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),odin2thor)
+
+$(call add-radio-file-sha1-checked,radio/abl.img,0380e23ab6c89c16a7398fb9843b21f6bdfcac20)
+$(call add-radio-file-sha1-checked,radio/aop.img,40b9fa85f91c5f32ce089622db2ea6542ef91f22)
+$(call add-radio-file-sha1-checked,radio/aop_config.img,0617eba53e77f38f573c8db04ca2a30d677693b0)
+$(call add-radio-file-sha1-checked,radio/bluetooth.img,efd49b4bea104d403fa7e820ab52b2bf9a704c9c)
+$(call add-radio-file-sha1-checked,radio/cpucp.img,140cbcc458d38d9b23c24282635b837ec1862868)
+$(call add-radio-file-sha1-checked,radio/devcfg.img,e17e187780511787da4571ef6dd4a13c4e273f34)
+$(call add-radio-file-sha1-checked,radio/dsp.img,4c6673195ac4058abdeac2ff7e4203c217f37e26)
+$(call add-radio-file-sha1-checked,radio/hyp.img,85600eecfaa9a998f4c3981931844b4ea0e867ec)
+$(call add-radio-file-sha1-checked,radio/imagefv.img,362044fae4d4e8a7deb10c675f007510b9b366cd)
+$(call add-radio-file-sha1-checked,radio/keymaster.img,62b8b65b119a2074fdc09052fcc869e2f8d907ad)
+$(call add-radio-file-sha1-checked,radio/modem.img,f0f5179e3c8d5337b603a6f38f210dbe07f2fabb)
+$(call add-radio-file-sha1-checked,radio/multiimgoem.img,e4469be87d1fb7719b871ab5c2445fcf703f072c)
+$(call add-radio-file-sha1-checked,radio/multiimgqti.img,ef0d3fea9529b1905e7955edfa6718182ed42f48)
+$(call add-radio-file-sha1-checked,radio/qupfw.img,6ac0744de8bde3a6f5c4f53cba8ef431b6c8cc42)
+$(call add-radio-file-sha1-checked,radio/shrm.img,6d49bd7bc2dd537628b08a738567b33fc958d9d0)
+$(call add-radio-file-sha1-checked,radio/tz.img,37bc472cd3b7f57ceb17d3f4a110ae7fb55c558d)
+$(call add-radio-file-sha1-checked,radio/uefi.img,ba8ef2e6fd30c6fbdf1b324ebddcfc2ff8939582)
+$(call add-radio-file-sha1-checked,radio/uefisecapp.img,9f94e7bc98face6615f27a4e041034d050b8ba3c)
+$(call add-radio-file-sha1-checked,radio/xbl.img,fd0eeed990bd50a9e8220f4b26149b193465754b)
+$(call add-radio-file-sha1-checked,radio/xbl_config.img,d41ca4e8a6ce61b0ebf60e7d9b8a7d1d44e86565)
+$(call add-radio-file-sha1-checked,radio/xbl_ramdump.img,47f3c50da01248c9295a6de570cbf0f18776d4bf)
+
+endif
---
+++ odin2thor-vendor.mk
@@ -0,0 +1,75 @@
+#
+# Automatically generated file. DO NOT MODIFY
+#
+
+PRODUCT_SOONG_NAMESPACES += \
+    vendor/ayn/odin2thor
+
+PRODUCT_COPY_FILES += \
+    vendor/ayn/odin2thor/proprietary/vendor/etc/display/qdcm_calib_data_ch13726a_video_mode_dsi_boe_panel_with_DSC.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/qdcm_calib_data_ch13726a_video_mode_dsi_boe_panel_with_DSC.json \
+    vendor/ayn/odin2thor/proprietary/vendor/etc/display/qdcm_calib_data_icna3520_amoled_panel_with_DSC.json:$(TARGET_COPY_OUT_VENDOR)/etc/display/qdcm_calib_data_icna3520_amoled_panel_with_DSC.json \
+    vendor/ayn/odin2thor/proprietary/vendor/etc/sensors/config/kailua_hdk_sh5001_0.json:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/config/kailua_hdk_sh5001_0.json \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b00:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b00 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b01 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b02 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b03:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b03 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b04:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b04 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b05:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b05 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b06:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b06 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b07:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b07 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b08:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b08 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b09:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b09 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b10:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b10 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b11:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b11 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b12:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b12 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b13:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b13 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b14:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b14 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b15:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b15 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b16:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b16 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b17:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b17 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b18:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b18 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b19:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b19 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b20:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b20 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b21:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b21 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b22:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b22 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b23:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b23 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b24:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b24 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b25:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b25 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b26:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b26 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b27:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b27 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b28:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b28 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b29:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b29 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b30:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b30 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b31:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b31 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b32:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b32 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b33:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b33 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b34:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b34 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b35:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b35 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b36:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b36 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b37:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b37 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b38:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b38 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b39:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b39 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b40:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b40 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b41:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b41 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b42:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b42 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b43:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b43 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b44:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b44 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b45:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b45 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b46:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b46 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b47:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b47 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b48:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b48 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b49:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b49 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b50:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b50 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b51:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b51 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b52:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b52 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b53:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b53 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b54:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b54 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b55:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b55 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.b56:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.b56 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp.mdt \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp_dtb.b00:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp_dtb.b00 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp_dtb.b01:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp_dtb.b01 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp_dtb.b02:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp_dtb.b02 \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/adsp_dtb.mdt:$(TARGET_COPY_OUT_VENDOR)/firmware/adsp_dtb.mdt \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/aw883xx_acf.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/aw883xx_acf.bin \
+    vendor/ayn/odin2thor/proprietary/vendor/firmware/mcuapp_firmware.bin:$(TARGET_COPY_OUT_VENDOR)/firmware/mcuapp_firmware.bin
